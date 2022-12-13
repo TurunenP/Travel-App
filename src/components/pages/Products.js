7791ed1ec6a125c20637835f3a18d349ffeef6fd
TurunenP <k7jepu00@students.oamk.fi>
2022-12-13 11:48:13 +0200

    First commit

---
+++ src/components/pages/Products.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../App.css';
+// function Products() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default Products
+
+export default function Products() {
+    return <h1 className='products'>PRODUCTS</h1>;
+  }
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import ReactDom from 'react-dom';
+// import './index.css';
+import App from './App';
+//import ReactDom from 'react-dom';
+//import React from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  //<React.StrictMode>
+    <App />
+  //{/* </React.StrictMode> */ }
+  
+);
+
+//ReactDOM.render(<App />, document.getElementById('root'));
